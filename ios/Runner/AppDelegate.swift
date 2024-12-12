96e76729adc8645fcfd4ec53fe95b91237f52b40
Sang Nguyen Tan <53396533+COCONUC@users.noreply.github.com>
2024-12-12 18:52:03 +0700

    Fix tensor.dart lib

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import Flutter
 import MLKitFaceDetection
 import MLKitVision
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
  override func application(
    _ application: UIApplication,
